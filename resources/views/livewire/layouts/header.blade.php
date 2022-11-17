63c7409fcef9cc0da88e062c4f6a94b7a81bdb02
10994015 <a0938599191@gmail.com>
2022-11-18 01:53:34 +0800

    1118

--- resources/views/livewire/layouts/header.blade.php
+++ resources/views/livewire/layouts/header.blade.php
@@ -2,7 +2,7 @@
     <a href="/" class="logo">DYM遊戲</a>
     <form action="/logout" method="post">
         @csrf
-        {{Auth::user()->name}}
+        {{--Auth::user()->name--}}
         <button type="submit" class="btn btn-light logout" >登出</button>
     </form>
 </div>
